a2e17b754ba966ba0f1ac08b65930d63b13597cb
erekenoob <erekenoob@gmail.com>
2026-09-20 19:29:21 +0500

    feat: create abstract Logistics creator with shared delivery workflow

---
+++ src/main/java/logistics/Logistics.java
@@ -0,0 +1,11 @@
+package logistics;
+
+public abstract class Logistics {
+
+    protected abstract Transport createTransport();
+
+    public void planDelivery(String cargo, String destination) {
+        Transport transport = createTransport();
+        transport.deliver(cargo, destination);
+    }
+}
